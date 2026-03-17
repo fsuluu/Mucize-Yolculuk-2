3d0155171d53137cf9ef42a0ee7c7791e5a9d1bf
fsuluu <fatmasulu12@gmail.com>
2026-03-18 00:35:02 +0300

    Dosyalar güncellendi

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mucizeyolculuk2.app',
+  appName: 'MucizeYolculuk2',
+  webDir: 'dist'
+};
+
+export default config;
